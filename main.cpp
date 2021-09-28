7fe5804f3835286018411f801b7f4ffd880fa65f
Pablo Escobar <77400177+esc20936@users.noreply.github.com>
2021-09-28 12:04:58 -0600

    Declaración mutex
    Se agregaron las instrucciones para el funcionamiento del mutex y se corrigió error de lógica en el ciclo del pthread_join

--- main.cpp
+++ main.cpp
@@ -21,17 +21,20 @@ pthread_mutex_t lock;
 
 int main() {     
     pthread_t th[100];  // Declare the vector that contains all the threeads
-
+    //Initialize mutex
+   if(pthread_mutex_init(&lock,NULL)!=0){
+        printf("\nERROR: Mutex init failed");
+        return 1;
+    }
 
     //Inicializar variables
     int numberOfThread;
     //Pedir el numero de threads
     cout << "Ingrese el numero de hilos que desea (del 1 al 100): " ;
     cin >> numberOfThread;  
+ 
 
-
-
-     //Condicional por si incerta mal el hilo 
+     //Condicional por si incerta mal la cantidad de hilos 
     if (numberOfThread > 100 or numberOfThread<1){
 
         cout << "[ERROR] Ingrese un valor de hilos validos (Entre 1 y 100)";
@@ -41,21 +44,40 @@ int main() {
         {
 
             // * Initialize the thread and search for errors
-            if (pthread_create(&th[i], NULL, &series_pi, NULL) != 0) {
+            if (pthread_create(&th[i], NULL, &series_pi, (void *) i) != 0) {
                 printf("[ERROR] - Creando hilos");
             }
             printf("Hilo %d ha comenzado\n", i);   
                 
         }
+    
         // * Wait for all the threads to finish
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < numberOfThread; i++)
         {
             pthread_join(th[i], NULL);
         }
+        pthread_mutex_destroy(&lock);
     
         // * Print the result
         printf("[RESULTADO] El valor de PI es: %d\n", pi);
+        
     }
 
 
+  return 0;
+
+
+}
+
+void* series_pi(void *params) {
+    //bloqueo del mutex
+    pthread_mutex_lock(&lock);
+    int i;
+    i= (int) params; 
+    //Proceso del metodo
+    pi+=i;
+
+    //Desbloqueo del mutex
+    pthread_mutex_unlock(&lock);
+    pthread_exit(0);
 }
